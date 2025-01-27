b2b2213ba147f1f20140d4931beba9f64bc51a3f
Carl Flood <carlflood@stripe.com>
2025-01-27 12:02:13 -0600

    logout fix

--- app/[locale]/(dashboard)/settings/layout.tsx
+++ app/[locale]/(dashboard)/settings/layout.tsx
@@ -45,7 +45,11 @@ export default function SettingsLayout({
             <Button
               className="text-md ml-2 self-end p-2 hover:bg-white/80"
               variant="ghost"
-              onClick={() => signOut({callbackUrl: '/default/login'})}
+              onClick={() =>
+                signOut({
+                  callbackUrl: `${new URL(window.location.href).origin}/default/login`,
+                })
+              }
             >
               {t('dashboard.settings.sign_out')}
             </Button>
