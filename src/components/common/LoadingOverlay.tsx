e86b748e2216c95c5d6576ca077ca1e1f6e90644
Ryan Huellen <rwh@stripe.com>
2026-01-09 11:31:59 -0600

    fix: loading overlay colors

--- src/components/common/LoadingOverlay.tsx
+++ src/components/common/LoadingOverlay.tsx
@@ -4,7 +4,7 @@ import { LoadingSpinner } from './LoadingSpinner';
 
 export const LoadingOverlay = () => {
   return (
-    <div className='absolute inset-0 flex items-center justify-center min-h-screen bg-brand-secondary'>
+    <div className='absolute inset-0 flex items-center justify-center min-h-screen bg-white'>
       <div className='text-center'>
         <LoadingSpinner className='text-brand-primary' />
         <p className='mt-4 text-lg font-semibold text-brand-primary'>
