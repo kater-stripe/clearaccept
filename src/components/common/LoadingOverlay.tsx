774fa1b4ab7008002591818c80ff81c0b7e5b999
Ryan Huellen <rwh@stripe.com>
2026-01-13 13:27:38 -0800

    feat: better landing

--- src/components/common/LoadingOverlay.tsx
+++ src/components/common/LoadingOverlay.tsx
@@ -4,7 +4,7 @@ import { LoadingSpinner } from './LoadingSpinner';
 
 export const LoadingOverlay = () => {
   return (
-    <div className='absolute inset-0 flex items-center justify-center min-h-screen bg-white'>
+    <div className='absolute inset-0 flex items-center justify-center min-h-screen bg-black'>
       <div className='text-center'>
         <LoadingSpinner className='text-brand-primary' />
         <p className='mt-4 text-lg font-semibold text-brand-primary'>
