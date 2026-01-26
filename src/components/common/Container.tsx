634f62960ea0e32fdc01489b63eaf5e58e425430
Ryan Huellen <rwh@stripe.com>
2026-01-26 17:27:17 -0600

    feat: using new tools panel

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -9,7 +9,7 @@ export const Container = ({
 }: ContainerProps) => {
   return (
     <div
-      className={`mx-auto w-full  max-w-7xl py-8 px-4 sm:px-6 lg:px-8 ${className}`}
+      className={`mx-auto w-full min-h-screen max-w-7xl py-8 px-4 sm:px-6 lg:px-8 ${className}`}
       {...props}
     >
       {children}
--- src/constants/toolsPanelTabs.ts
+++
@@ -1,8 +0,0 @@
-export const TOOLS_PANEL_TABS = [
-  'Appearance',
-  'Account',
-  'Seeding',
-  'Financing',
-] as const;
-
-export type ToolsPanelTab = (typeof TOOLS_PANEL_TABS)[number];
